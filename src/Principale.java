b0255969547e614093a143fc6ab64f552239c4fd
fabrice <bazire.fabrice@gmail.com>
2019-05-01 11:41:41 +0200

    code commenté

--- src/Principale.java
+++ src/Principale.java
@@ -22,6 +22,7 @@ public class Principale {
             while((s = b.readLine())!=null){
                 Scanner sc = new Scanner (s);
                 sc.useDelimiter("\n");
+                //La chaine de caractere s, a pour but de recuperer la ligne en cours de lecture du fichier, et le while fais en sorte que tant que le fichier n'est pas a la fin, s prend la ligne suivante
                 try {
                     i = sc.next();
                     //code de lecture
@@ -32,18 +33,18 @@ public class Principale {
                     if (i.equals("fin")){
                         return arbre;
                     }
-                    x = i.split(" ");
-                    if (x[0].charAt(0) == '*'){
+                    x = i.split(" "); // Cette commande sert a transformer la chaine de caractere qui contient toute la ligne en un tableau de mot pour analyser mot par mot
+                    if (x[0].charAt(0) == '*'){ //Ici on regarde si le premier carcatere du premier mot de la ligne est une etoile
                         if (x[1].equals("fin")){
                             arbre = arbre.getPere();
                         }
-                        if (x[2].equals("d")) {
+                        if (x[2].equals("d")) { //Ici on regarde si le deuxieme mot est un d, dans ce cas on doit créer un dossier
                             arbre.add(new ArbreFichiers(null,null,null,null, x[1], false, null, 0));
                             tmp = arbre;
                             arbre = arbre.getFils1();
                             arbre.setPere(tmp);
                         }
-                        if (x[2].equals("f")){
+                        if (x[2].equals("f")){ //Ici on regarde si le deuxieme mot est un f, dans ce cas on doit créer un fichier
                             contenu = b.readLine();
                             arbre.add(new ArbreFichiers(null,null,null,null, x[1], true, contenu, contenu.length()));
                         }
@@ -64,7 +65,8 @@ public class Principale {
     //etape 3
     //Méthode qui selon la commande passée en paramètre va l'éxécuter sur l'arbreFichiers en paramètre
     public static void implemente_commandes (ArbreFichiers a, String s){
-        String[] x = s.split(" ");
+        String[] x = s.split(" "); //Comme dans la methode du dessus, on transforme la chaine s (qui ici est la commande tapée par l'utilisateur) en tableau de mots
+        //Ensuite on fais ce qu'il faut par rapport a la commande entrée par l'utilisateur
         if (x[0].equals("ls")){
             System.out.println(a.info());
         }
@@ -80,8 +82,8 @@ public class Principale {
         }
         if (x[0].equals("mkfile")){
             System.out.println("Tapez le contenu de votre fichier !");
-            String g = in.nextLine();
-            a.add(new ArbreFichiers(a,null,null,null, x[1], true, g, g.length()));
+            String g = in.nextLine(); // l'utilisateur entre le contenu du fichier qu'il veur créer
+            a.add(new ArbreFichiers(a,null,null,null, x[1], true, g, g.length())); //on crée le fichier avec le nom et le contenu que l'utilisateur a choisi
         }
         if (x[0].equals("less")){
             if (a.acces(x[1]) == null){
@@ -91,18 +93,18 @@ public class Principale {
                 String [] contenu = a.getFils1().getContenu().split(" ");
                 String res = "";
                 for (int i = 0; i < contenu.length; i++){
-                    if (contenu[i].charAt(0) == '_' && contenu[i].charAt(1) == '_' && contenu[i].charAt(2) == '_'){
+                    if (contenu[i].charAt(0) == '_' && contenu[i].charAt(1) == '_' && contenu[i].charAt(2) == '_'){ //si les 3 premiers caracteres sont des _, alors on saute une ligne puis on affiche le contenu de la ligne
                         res += "\n";
                         for (int w = 3; w < contenu[i].length(); w++){
                             res += Character.toString(contenu[i].charAt(w));
                         }
                     }else{
-                        if (contenu[i].charAt(contenu[i].length()-1) == '_' && contenu[i].charAt(contenu[i].length()-2) == '_' && contenu[i].charAt(contenu[i].length()-3) == '_'){
+                        if (contenu[i].charAt(contenu[i].length()-1) == '_' && contenu[i].charAt(contenu[i].length()-2) == '_' && contenu[i].charAt(contenu[i].length()-3) == '_'){ // si les 3 derniers caractere sont des _, alors on affiche la ligne puis on saute une ligne
                             for (int w = 0; w < contenu[i].length()-3; w++){
                                 res += Character.toString(contenu[i].charAt(w));
                             }
                             res += "\n";
-                        }else {
+                        }else { // et sinon on affiche simplement la ligne
                             res += contenu[i] + " ";
                         }
                     }
@@ -131,13 +133,13 @@ public class Principale {
             }
         }
         String commande= "";
-        while (! commande.equals("quit") || ! commande.equals("exit")){
+        while (true){ //on boucle infinie pour demander une commande a l'utilisateur
             System.out.println ("Veuillez entrer votre commande");
             commande = in.nextLine();
             if (commande.equals("rm") || commande.equals("pwd") || commande.equals("less") || commande.equals("mkfile") || commande.equals("mkdir") || commande.equals("cd") || commande.equals("ls")){
                 implemente_commandes(arbre, commande);
             }else{
-                if (commande.equals("quit")|| commande.equals("exit")){
+                if (commande.equals("quit")|| commande.equals("exit")){ //Le programme s'arrete seulement si l'utilisateur entre quit ou exit
                     break;
                 }else {
                     System.out.println("commande invalide");
